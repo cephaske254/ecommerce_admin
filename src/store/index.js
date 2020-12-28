60220ad2a56ceca88bf95f7577cacb25936bef69
cephaske254 <cephaske254@gmail.com>
2020-12-28 19:14:09 +0300

    hot reload modules vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,40 @@
-import { createStore, mapState } from "vuex";
+import Vuex, { createStore, mapState } from "vuex";
+import mutations from "./mutations";
 
-export default createStore({
-  state: {},
-  mutations: {},
+const store = new Vuex.Store({
+  state: {
+    products: [],
+  },
+  mutations,
   actions: {},
-  modules: {},
+  modules,
 });
+export default store;
+
+const { context, modules } = loadModules();
+function loadModules() {
+  const context = require.context("./modules", false, /([a-z_]+)\.js$/i);
+
+  const modules = context
+    .keys()
+    .map((key) => ({ key, name: key.match(/([a-z_]+)\.js$/i)[1] }))
+    .reduce(
+      (modules, { key, name }) => ({
+        ...modules,
+        [name]: context(key).default,
+      }),
+      {}
+    );
+
+  return { context, modules };
+}
+
+if (module.hot) {
+  module.hot.accept(context.id, () => {
+    const { modules } = loadModules();
+
+    store.hotUpdate({
+      modules,
+    });
+  });
+}
