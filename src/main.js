98adecc32c8c15621facbfc2008974e0e8fd334b
cephaske254 <cephaske254@gmail.com>
2020-12-28 18:09:17 +0300

    add vuex store

--- src/main.js
+++ src/main.js
@@ -6,10 +6,11 @@ import DataGridAbstract from "@/subcomponents/DataGrid.abstract.vue";
 import mixins from "@/utils/mixins";
 
 import Axios from "axios";
+import store from './store'
 
 window.axios = Axios;
 
-createApp(App)
+createApp(App).use(store)
   .mixin(mixins)
   .component("data-list", DataListAbstract)
   .component("data-grid", DataGridAbstract)
